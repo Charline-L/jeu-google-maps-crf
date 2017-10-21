85f50b4931724d1296261e75aacf39fc98278019
charline <hello@charline-laporte.fr>
2017-10-21 19:18:00 +0200

    'init map'

--- src/all.js
+++ src/all.js
@@ -1,20 +1,30 @@
-class GMap {
-  constructor(){
-    let t = this;
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var GMap = function () {
+  function GMap() {
+    _classCallCheck(this, GMap);
+
+    var t = this;
     t.mapContainer = $('#map')[0];
   }
-  init(){
-    let t = this;
-    let map = new google.maps.Map(t.mapContainer, {
-      center: {lat: -34.397, lng: 150.644},
-<<<<<<< HEAD
-      zoom: 8
-=======
-      zoom: 16
->>>>>>> master
-    });
-  }
-}
 
-const map = new GMap();
+  _createClass(GMap, [{
+    key: 'init',
+    value: function init() {
+      var t = this;
+      var map = new google.maps.Map(t.mapContainer, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      });
+    }
+  }]);
+
+  return GMap;
+}();
+
+var map = new GMap();
 GMap.init;
