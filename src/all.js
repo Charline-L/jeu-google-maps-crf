a7067670312c1698ba352ab173dbbd11f4aa6494
charline <hello@charline-laporte.fr>
2017-10-21 19:21:13 +0200

    'map init'

--- src/all.js
+++ src/all.js
@@ -1,38 +1,30 @@
-class Bonjour {
-  constructor() {
-    let t= this;
-    t.bonjour= 'Bonjour toi !';
-  }
-  init() {
-    let t= this;
-    console.log(t.bonjour);
-  }
-}
+'use strict';
 
-class GMap {
-  constructor(){
-    let t = this;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var GMap = function () {
+  function GMap() {
+    _classCallCheck(this, GMap);
+
+    var t = this;
     t.mapContainer = $('#map')[0];
   }
-  init(){
-    let t = this;
-    let map = new google.maps.Map(t.mapContainer, {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 16
-    });
-  }
-}
 
-class GMap {
-  constructor(){
-    let t = this;
-    t.mapContainer = $('#map')[0];
-  }
-  init(){
-    let t = this;
-    let map = new google.maps.Map(t.mapContainer, {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-  }
-}
+  _createClass(GMap, [{
+    key: 'init',
+    value: function init() {
+      var t = this;
+      var map = new google.maps.Map(t.mapContainer, {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 16
+      });
+    }
+  }]);
+
+  return GMap;
+}();
+
+var map = new GMap();
+GMap.init;
